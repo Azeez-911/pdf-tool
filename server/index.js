32339ebd9b71cf1284c7efe106556e638df6588a
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-05-05 23:52:07 +0530

    Initial backend setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const pdfRoutes = require("./routes/pdfRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/pdf", pdfRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
